4af81f9198391c72d621d5e53b7e8c3821d0b88f
fengwen <1477452395@qq.com>
2019-10-07 22:20:18 +0800

    提交

---
+++ src/test/java/com/feng/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feng.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
